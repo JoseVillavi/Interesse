ea95deac88e610bced23b505fff0a8b998dbd931
JoseVillavi <joselopezvillavicencio@gmail.com>
2025-05-10 00:27:24 -0600

    add first function

---
+++ main/base_test.py
@@ -0,0 +1,27 @@
+from playwright.async_api import async_playwright
+from locators import AmazonLocators
+
+async def iniciar_sesion_amazon():
+    async with async_playwright() as p:
+        await page.goto(AmazonLocators.AmazonWeb)
+
+        # Iniciar sesión
+        await page.click(AmazonLocators.Cuenta_menu)
+
+        # API
+        response = requests.get("http://localhost:8000/login")
+
+        assert response.status_code == 200, "La conexión con la API falló"
+
+        data = response.json()
+        usuario = data["Username"]
+        contrasena = data["Password"]
+
+        # Login function
+        await page.click(AmazonLocators.Usuario_campo)
+        await page.fill(AmazonLocators.Usuario_campo, usuario)
+        await page.click(AmazonLocators.Boton_usuario)
+        await page.click(AmazonLocators.Contraseña_campo)
+        await page.fill(AmazonLocators.Contraseña_campo, contrasena)
+        await page.click(AmazonLocators.Boton_enviar_cuenta)
+
